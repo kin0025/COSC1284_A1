aeb8ed6263508b476b053ca69f71073299650e66
alex@akinrosssmith.id.au <xW0bhUH@w>
2016-03-15 14:22:56 +1100

    Added optimise pathing, working on a better vertical pathing solution.

--- src/RobotControl.java
+++ src/RobotControl.java
@@ -138,17 +138,25 @@ controlMechanismOptimisedB(barHeights, blockHeights);
         }
     }
     public void controlMechanismOptimisedB(int barHeights[], int blockHeights[]) {
+        //todo -  once i know how to use for loops, get rid of some of these horrible while loops
         int height = 2;         // Initial height of arm 1
         int width = 1;         // Initial width of arm 2
         int drop = 0;         // Initial depth of arm 3
-        int NumberBlocks = 4;
-        int NoThreeBlocks = blockHeights.length; // Count the number of blocks- I need to count only 3s
+        int NumberBlocks = blockHeights.length; //How many blocks need to be moved.
+        int NoThreeBlocks = 0; // Count the number of blocks that need to be put on bars
+        byte ThreesLoop = 0;
+        while(ThreesLoop != blockHeights.length){
+            if(blockHeights[ThreesLoop] == 3){
+                NoThreeBlocks++;
+            }
+            ThreesLoop++;
+        }
         int ThreesDropBarNumber = 9 - NoThreeBlocks;
+       //Actually usable Optimised stuff
+        int[] BarOptimised = optimisePathing(barHeights);
         //setting stack height total for the end.
         int StackHeight = 0;
         int BlockRuns = blockHeights.length;
-        //Actually useable Optimised stuff
-        int[] BarOptimised = optimisePathing(barHeights);
         while(BlockRuns != 0) {
             StackHeight += blockHeights[BlockRuns - 1];
 
@@ -163,7 +171,7 @@ controlMechanismOptimisedB(barHeights, blockHeights);
             BarRuns--;
         }
         height = moveVerticalTo(13,height);
-        while(NumberBlocks !=0) {
+        while(NumberBlocks !=0) { //Doing the actual movement
             //Figure out the height of the current block in the array
             int TopBlockNumber = blockHeights.length;
             int CurrentBlock = blockHeights[TopBlockNumber - 1];
@@ -175,7 +183,7 @@ controlMechanismOptimisedB(barHeights, blockHeights);
             r.pick();
             //move to a spot where we can safely change our vertical height- legacy code from Part A and not necessary - a soltution that does not require the horizontal move could be devised, but in number of moves is equivalent, and this is cleaner to see, but it works
             width = moveHorizontalTo(9, width);
-            drop = moveCraneTo(2, drop);
+            drop = moveCraneTo(2, drop); //todo- add a check that only moves to the max height along our path
             StackHeight = StackHeight - 3;
             width = moveHorizontalTo(ThreesDropBarNumber, width);
             int currentBarHeight = barHeights[ThreesDropBarNumber - 3];
@@ -235,7 +243,6 @@ controlMechanismOptimisedB(barHeights, blockHeights);
         }
         return (Position); //Return our position so that it stays up to date
     }
-
     public int moveCraneTo(int MoveTo, int Position) { //Semi Obsolete- the move crane to position replaces this, but this still has uses
         while (Position != MoveTo) { //Why am I still writing comments for stuff that is the same as before?
             if (MoveTo < 13 && MoveTo > -1) {
@@ -280,86 +287,94 @@ controlMechanismOptimisedB(barHeights, blockHeights);
         }
         return (Drop);
     }
-
     public int[] optimisePathing(int barHeights[]) { //literally broken, need more debugging
         int BarRuns = 0; // We can assume that there are always 6 bars
-        int[] BarNumbers = {0,0,0,0,0,0};
-        while(BarRuns <= barHeights.length - 1) {
-            int[] OptimisationBars = {0,0,0,0,0,0,0};
-            OptimisationBars[BarRuns] = (7-barHeights[BarRuns])+(6-BarRuns);
-            int[] Optimisation = {20,20,20,20,20,20,20};
-            byte r = 0;
-            if(OptimisationBars[BarRuns] <= Optimisation[0]){
-                while(r <= 6){
-                    Optimisation[r+1]=Optimisation[r];
-                    BarNumbers[r+1]=BarNumbers[r];
-                    r++;
+        int[] BarNumbers = {0, 0, 0, 0, 0, 0, 0};
+        int[] OptimisationBars = {0, 0, 0, 0, 0, 0, 0};
+        int[] Optimisation = {21, 22, 23, 24, 25, 26, 27};
+        while (BarRuns <= 5) {
+            OptimisationBars[BarRuns] = (7 - barHeights[BarRuns]) + (6 - BarRuns); //This should give {9,11,9,7,3,7}
+            //End result should be {4,5,6,0/2,2/0,1}
+            BarRuns++;
+        }
+        BarRuns = 0;
+        while (BarRuns <= 5) {
+            byte r = 5;
+            // Check the current number against all positions in optimisation.
+            if (OptimisationBars[BarRuns] <= Optimisation[0]) {// Is the number currently been examined less than the first position in the optimisation array?
+                while (r >= 0) { // If it is, move all numbers after it down an array index. Do the same to the BarNumbers array
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
                 }
-                Optimisation[0]=OptimisationBars[BarRuns];
-                BarNumbers[0]=BarNumbers[BarRuns];
-            }
-            else if(OptimisationBars[BarRuns] <= Optimisation[1]){
-                r = 1;
-                while(r <= 5){
-                    Optimisation[r+1]=Optimisation[r];
-                    BarNumbers[r+1]=BarNumbers[r];
-                    r++;
+                Optimisation[0] = OptimisationBars[BarRuns]; // Fill the spot left after moving all the numbers down
+                BarNumbers[0] = BarRuns; // Record the Bar associated with this optimisation value.
+            } else if (OptimisationBars[BarRuns] <= Optimisation[1]) {
+                while (r >= 1) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
                 }
-                Optimisation[1]=OptimisationBars[BarRuns];
-                BarNumbers[1]=BarNumbers[BarRuns];
-            }
-            else if(OptimisationBars[BarRuns] <= Optimisation[2]){
-                r = 2;
-                while(r <= 4){
-                    Optimisation[r+1]=Optimisation[r];
-                    BarNumbers[r+1]=BarNumbers[r];
-                    r++;
+                Optimisation[1] = OptimisationBars[BarRuns];
+                BarNumbers[1] = BarRuns;
+            } else if (OptimisationBars[BarRuns] <= Optimisation[2]) {
+                while (r >= 2) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
                 }
-                Optimisation[2]=OptimisationBars[BarRuns];
-                BarNumbers[2]=BarNumbers[BarRuns];
-            }
-            else if(OptimisationBars[BarRuns] <= Optimisation[3]){
-                r = 3;
-                while(r <= 3){
-                    Optimisation[r+1]=Optimisation[r];
-                    BarNumbers[r+1]=BarNumbers[r];
-                    r++;
+                Optimisation[2] = OptimisationBars[BarRuns];
+                BarNumbers[2] = BarRuns;
+            } else if (OptimisationBars[BarRuns] <= Optimisation[3]) {
+                while (r >= 3) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
                 }
-                Optimisation[3]=OptimisationBars[BarRuns];
-                BarNumbers[3]=BarNumbers[BarRuns];
-            }
-            else if(OptimisationBars[BarRuns] <= Optimisation[4]){
-                r = 4;
-                while(r <= 2){
-                    Optimisation[r+1]=Optimisation[r];
-                    BarNumbers[r+1]=BarNumbers[r];
-                    r++;
+                Optimisation[3] = OptimisationBars[BarRuns];
+                BarNumbers[3] = BarRuns;
+            } else if (OptimisationBars[BarRuns] <= Optimisation[4]) {
+                while (r >= 4) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
                 }
-                Optimisation[4]=OptimisationBars[BarRuns];
-                BarNumbers[4]=BarNumbers[BarRuns];
-            }
-            else if(OptimisationBars[BarRuns] <= Optimisation[5]){
-                r = 5;
-                while(r <= 1){
-                    Optimisation[r+1]=Optimisation[r];
-                    BarNumbers[r+1]=BarNumbers[r];
-                    r++;
+                Optimisation[4] = OptimisationBars[BarRuns];
+                BarNumbers[4] = BarNumbers[BarRuns];
+            } else if (OptimisationBars[BarRuns] <= Optimisation[5]) {
+                while (r >= 5) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
                 }
-                Optimisation[5]=OptimisationBars[BarRuns];
-                BarNumbers[5]=BarNumbers[BarRuns];
+                Optimisation[5] = OptimisationBars[BarRuns];
+                BarNumbers[5] = BarNumbers[BarRuns];
             }
             BarRuns++;
         }
         //convert the bar numbers array to coordinates
         int x = 0;
-while(x <= 5){
-    BarNumbers[x] = BarNumbers [x]+3;
-    x++;
-}
-        return(BarNumbers);
+        while (x <= 5) {
+//            BarNumbers[x] = BarNumbers[x]+3;
+            System.out.print(BarNumbers[x] + ",");
+            x++;
+
+        }
+        return (BarNumbers);
     }
+    public int checkMaxPathingHeight(int barHeights[], int StartBar, int EndBar){
+        byte BarCount = 0;
+        int MaximumHeight = 0;//todo- make it only run for bars between StartBar and EndBar
+     while(barHeights.length >= BarCount){
+         if(barHeights[BarCount] >= MaximumHeight){
+             MaximumHeight = barHeights[BarCount];
+         }
+         BarCount++;
+     }
 
 
-} 
+        return(MaximumHeight);
+    }
+}
 
 
