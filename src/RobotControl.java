cf2d21c522e91d681bbd68e419dd4d0e964a5e07
alex@akinrosssmith.id.au <xW0bhUH@w>
2016-03-15 22:21:39 +1100

    Added legacy support so older iterations don't stop compile from happening

--- src/RobotControl.java
+++ src/RobotControl.java
@@ -14,11 +14,12 @@ class RobotControl {
     public void control(int barHeights[], int blockHeights[]) {//sampleControlMechanism(barHeights,blockHeights);
 //controlMechanismLoopedA(barHeights, blockHeights);
 //controlMechanismLoopedB(barHeights, blockHeights);
-        controlMechanismOptimisedB(barHeights, blockHeights);
-
+//        controlMechanismOptimisedB(barHeights, blockHeights);
+        controlMechanismC(barHeights, blockHeights);
 
     }
 
+    //Earlier ControlMechanism's may not work, as they reference methods shared with later methods that may have changed in implementation.
     public void controlMechanismBasicA(int barHeights[], int blockHeights[]) {
         int height = 2;         // Initial height of arm 1
         int width = 1;         // Initial width of arm 2
@@ -219,19 +220,23 @@ class RobotControl {
         //todo -  once i know how to use for loops, get rid of some of these horrible while loops
         //todo- pathing properly. Don't asume all bars have a size 3 block on top.
         //todo- Path around bars. ensure that bars will not block the path of other blocks and take that into account when choosing optimal bar to place on.
+        //todo- Add actual comments for newer pathing stuff.
         int height = 2;         // Initial height of arm 1
         int width = 1;         // Initial width of arm 2
         int drop = 0;         // Initial depth of arm 3
         int NumberBlocks = blockHeights.length; //How many blocks need to be moved.
         int NoThreeBlocks = 0; // Count the number of blocks that need to be put on bars
-        byte ThreesLoop = 0;
+        byte ThreesLoop = 0;//todo-  change the number of referenced blocks in OptimisePathing to use Number of threes.
         while (ThreesLoop != blockHeights.length) {
             if (blockHeights[ThreesLoop] == 3) {
                 NoThreeBlocks++;
             }
             ThreesLoop++;
         }
-        int[] BarOptimised = optimisePathing(barHeights);
+        int[] BarOptimised = null;
+        if (NoThreeBlocks >= 1) {
+            BarOptimised = optimisePathing(barHeights, NoThreeBlocks);
+        }
         //setting stack height total for the end.
         int StackHeight = 0;
         int BlockRuns = blockHeights.length;
@@ -384,6 +389,131 @@ class RobotControl {
         return (Drop);
     }
 
+    public int[] optimisePathing(int barHeights[], int NumberOfThrees) { //literally broken, need more debugging
+        int BarRuns = 0; // We can assume that there are always 6 bars
+        int[] BarNumbers = {0, 0, 0, 0, 0, 0, 0};
+        int[] OptimisationBars = {0, 0, 0, 0, 0, 0, 0};
+        int[] Optimisation = {21, 22, 23, 24, 25, 26, 27};
+        while (BarRuns <= 5) {
+            OptimisationBars[BarRuns] = (7 - barHeights[BarRuns]) + (6 - BarRuns); //This should give {9,11,9,7,3,7}
+            //End result should be {4,5,6,0/2,2/0,1}
+            BarRuns++;
+        }
+        BarRuns = 0;
+        while (BarRuns <= 5) {
+            byte r = 5;
+            // Check the current number against all positions in optimisation.
+            if (OptimisationBars[BarRuns] <= Optimisation[0]) {// Is the number currently been examined less than the first position in the optimisation array?
+                while (r >= 0) { // If it is, move all numbers after it down an array index. Do the same to the BarNumbers array
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
+                }
+                Optimisation[0] = OptimisationBars[BarRuns]; // Fill the spot left after moving all the numbers down
+                BarNumbers[0] = BarRuns; // Record the Bar associated with this optimisation value.
+            } else if (OptimisationBars[BarRuns] <= Optimisation[1]) {
+                while (r >= 1) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
+                }
+                Optimisation[1] = OptimisationBars[BarRuns];
+                BarNumbers[1] = BarRuns;
+            } else if (OptimisationBars[BarRuns] <= Optimisation[2]) {
+                while (r >= 2) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
+                }
+                Optimisation[2] = OptimisationBars[BarRuns];
+                BarNumbers[2] = BarRuns;
+            } else if (OptimisationBars[BarRuns] <= Optimisation[3]) {
+                while (r >= 3) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
+                }
+                Optimisation[3] = OptimisationBars[BarRuns];
+                BarNumbers[3] = BarRuns;
+            } else if (OptimisationBars[BarRuns] <= Optimisation[4]) {
+                while (r >= 4) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
+                }
+                Optimisation[4] = OptimisationBars[BarRuns];
+                BarNumbers[4] = BarNumbers[BarRuns];
+            } else if (OptimisationBars[BarRuns] <= Optimisation[5]) {
+                while (r >= 5) {
+                    Optimisation[r + 1] = Optimisation[r];
+                    BarNumbers[r + 1] = BarNumbers[r];
+                    r--;
+                }
+                Optimisation[5] = OptimisationBars[BarRuns];
+                BarNumbers[5] = BarNumbers[BarRuns];
+            }
+            BarRuns++;
+        }
+        //convert the bar numbers array to coordinates
+        int x = 0;
+        while (x <= 5) {
+//            BarNumbers[x] = BarNumbers[x]+3;
+            System.out.print(BarNumbers[x] + ",");
+            x++;
+
+        }
+        //For scenario 2 output should be 0,3,4,5,10
+        int[] NumbersFinal = {10, 10, 10, 10, 10};
+        int CurrentRun = NumberOfThrees - 1;
+        while (CurrentRun != -1) {
+            byte r = 3;
+            if (BarNumbers[CurrentRun] <= NumbersFinal[0]) {// Is the number currently been examined less than the first position in the optimisation array?
+                while (r >= 0) { // If it is, move all numbers after it down an array index. Do the same to the BarNumbers array
+                    NumbersFinal[r + 1] = NumbersFinal[r];
+                    r--;
+                }
+                NumbersFinal[0] = BarNumbers[CurrentRun]; // Record the Bar associated with this optimisation value.
+            } else if (BarNumbers[CurrentRun] <= NumbersFinal[1]) {// Is the number currently been examined less than the first position in the optimisation array?
+                while (r >= 1) { // If it is, move all numbers after it down an array index. Do the same to the BarNumbers array
+                    NumbersFinal[r + 1] = NumbersFinal[r];
+                    r--;
+                }
+                NumbersFinal[1] = BarNumbers[CurrentRun]; // Record the Bar associated with this optimisation value.
+            } else if (BarNumbers[CurrentRun] <= NumbersFinal[2]) {// Is the number currently been examined less than the first position in the optimisation array?
+                while (r >= 2) { // If it is, move all numbers after it down an array index. Do the same to the BarNumbers array
+                    NumbersFinal[r + 1] = NumbersFinal[r];
+                    r--;
+                }
+                NumbersFinal[2] = BarNumbers[CurrentRun]; // Record the Bar associated with this optimisation value.
+            } else if (BarNumbers[CurrentRun] <= NumbersFinal[3]) {// Is the number currently been examined less than the first position in the optimisation array?
+                while (r >= 3) { // If it is, move all numbers after it down an array index. Do the same to the BarNumbers array
+                    NumbersFinal[r + 1] = NumbersFinal[r];
+                    r--;
+                }
+                NumbersFinal[3] = BarNumbers[CurrentRun]; // Record the Bar associated with this optimisation value.
+            }
+            CurrentRun--;
+        }
+        return (NumbersFinal);
+
+    }
+
+    public int checkMaxPathingHeightToBars(int barHeights[], int LeftBar) {
+        int MaximumHeight = 0;//
+        if (LeftBar < 3) {
+            LeftBar = 3;
+        }
+        while (5 >= LeftBar - 3) {
+            if (barHeights[LeftBar - 3] >= MaximumHeight) {
+                MaximumHeight = barHeights[LeftBar - 3];
+            }
+            LeftBar++;
+        }
+
+
+        return (MaximumHeight);
+    }
+
     public int[] optimisePathing(int barHeights[]) { //literally broken, need more debugging
         int BarRuns = 0; // We can assume that there are always 6 bars
         int[] BarNumbers = {0, 0, 0, 0, 0, 0, 0};
@@ -457,9 +587,9 @@ class RobotControl {
             x++;
 
         }
-        //For scenario 2 output should be 0,3,4,5
+        //For scenario 2 output should be 0,3,4,5,10
         int[] NumbersFinal = {10, 10, 10, 10, 10};
-        byte CurrentRun = 3;
+        int CurrentRun = 3;
         while (CurrentRun != -1) {
             byte r = 3;
             if (BarNumbers[CurrentRun] <= NumbersFinal[0]) {// Is the number currently been examined less than the first position in the optimisation array?
@@ -491,23 +621,8 @@ class RobotControl {
         }
         return (NumbersFinal);
 
-    }
+    } //Only here for the older Methods.
 
-    public int checkMaxPathingHeightToBars(int barHeights[], int LeftBar) {
-        int MaximumHeight = 0;//
-        if (LeftBar < 3) {
-            LeftBar = 3;
-        }
-        while (5 >= LeftBar - 3) {
-            if (barHeights[LeftBar - 3] >= MaximumHeight) {
-                MaximumHeight = barHeights[LeftBar - 3];
-            }
-            LeftBar++;
-        }
-
-
-        return (MaximumHeight);
-    }
 }
 
 
